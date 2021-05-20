07374e4a59aae74456f8e7cc273c30381aabdc98
Azeddine Hamdaoui <ahamdaou@e2r9p6.1337.ma>
2021-05-20 19:44:42 +0100

    fast push

--- setup.sh
+++ setup.sh
@@ -8,24 +8,33 @@ if ! minikube status | grep -q 'host: Running'; then
 	minikube start
 fi
 
-# enabling addons
-minikube addons enable dashboard
-minikube addons enable metallb
+# enabling minikube addons
+minikube addons enable dashboard > /dev/null 1>&2
+minikube addons enable metallb > /dev/null 1>&2
 
 # launching dashboard
 echo "Launching dashboard..."
-minikube dashboard 2>/dev/null >/dev/null &
+minikube dashboard > /dev/null 1>&2 &
 
+# eval
+echo "eval docker env..."
 eval $(minikube docker-env)
 
-# Minikube IP
+# minikube IP
 IP=$(kubectl get node -o=custom-columns='DATA:status.addresses[0].address' | sed -n 2p)
-printf "Minikube IP: ${IP}"
+printf "Minikube IP: ${IP}\n"
 
 # Building images
 echo "Building images..."
 docker build -t wordpress $CONT/wordpress --build-arg IP=$IP
 docker build -t phpmyadmin $CONT/phpmyadmin --build-arg IP=$IP
+echo "$IP"
+
+# apply configmap
+kubectl apply -f $KUBE/configmap
 
 # launch pods
 kubectl apply -f $KUBE/pods
+
+# Last opening network in browser
+#open http://$IP
